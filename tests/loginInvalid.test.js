87be01442ed124861e2d4936dd6f33ddd541822d
Yustikaprwt <83440868+Yustikaprwt@users.noreply.github.com>
2025-05-06 17:12:15 +0800

    test(invalid_login): LGN_003 - Login account as user with problem user

--- tests/loginInvalid.test.js
+++ tests/loginInvalid.test.js
@@ -1,18 +1,20 @@
-const { By } = require("selenium-webdriver");
-const chrome = require("selenium-webdriver/chrome");
+const { By, until } = require("selenium-webdriver");
 const driver = require("../resources/driver.js");
 const assert = require("assert");
-const { locators, data, errorMessage } = require("../resources/locators.js");
+const {
+  locators,
+  data,
+  errorMessage,
+  expectedUrl,
+} = require("../resources/locators.js");
 require("dotenv").config();
 const BASE_URL = process.env.BASE_URL;
 
-describe("Login with invalid credentials", async () => {
-  //   this.timeout(30000);
+describe("Login with invalid credentials", async function () {
+  this.timeout(30000);
 
   before(async () => {
-    const option = new chrome.Options();
-
-    option.addArguments("--disable-save-password-bubble");
+    console.log("Running test login with invalid credentials");
   });
 
   it("LGN_002 - Login account as user with 'locked_out_user' username and valid password", async () => {
@@ -32,4 +34,52 @@ describe("Login with invalid credentials", async () => {
 
     assert.equal(getErrorMessage, expectedErrorMessage);
   });
+
+  it("LGN_003 - Login account as user with 'problem_user' username and valid password", async () => {
+    await driver.get(BASE_URL);
+
+    await driver
+      .findElement(By.id(locators.username))
+      .sendKeys(data.problemUser);
+    await driver.findElement(By.id(locators.password)).sendKeys(data.password);
+    await driver.findElement(By.id(locators.buttonLogin)).click();
+
+    const getUrl = await driver.getCurrentUrl();
+    const getExpectedUrl = await expectedUrl.dashboardUrl;
+    assert.equal(getUrl, getExpectedUrl);
+
+    const addBackpackButton = await driver.wait(
+      until.elementIsVisible(
+        await driver.wait(
+          until.elementLocated(By.id(locators.addBackpackButton))
+        )
+      ),
+      30000
+    );
+
+    await addBackpackButton.click();
+
+    await driver.wait(async () => {
+      const removeButton = await driver.findElement(
+        By.id(locators.removeBackpackButton)
+      );
+      const text = await removeButton.getText();
+      return text === "Remove";
+    }, 30000);
+
+    const removeBackpackButton = await driver.findElement(
+      By.id(locators.removeBackpackButton)
+    );
+    await removeBackpackButton.click();
+
+    await driver.wait(async () => {
+      const buttonText = await driver.findElement(
+        By.id(locators.removeBackpackButton)
+      );
+      const text = await buttonText.getText();
+      return text === "Remove";
+    }, 30000);
+  });
+
+  after(async () => await driver.close());
 });
