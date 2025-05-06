8daa6dcd62c481aadce32c20d90cba95fdf5fc7b
Yustikaprwt <83440868+Yustikaprwt@users.noreply.github.com>
2025-05-06 14:21:19 +0800

    update: add locators for error box allert

--- resources/locators.js
+++ resources/locators.js
@@ -3,13 +3,19 @@ export const locators = {
   password: "password",
   buttonLogin: "login-button",
   dashboardTitle: '//*[@data-test="title"]',
+  errorBoxAllert: '//*[@data-test="error"]',
 };
 
 export const data = {
   standardUser: "standard_user",
+  lockedOutUser: "locked_out_user",
   password: "secret_sauce",
   invalidPassword: "wrong_password",
   dashboardTitle: "Products",
+};
+
+export const errorMessage = {
+  lockedOutUser: "Epic sadface: Sorry, this user has been locked out.",
 };
 
 export const url = {
