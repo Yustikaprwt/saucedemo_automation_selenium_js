7936edc04c1ab7c426e5bd530d48297abbfeedb8
Yustikaprwt <83440868+Yustikaprwt@users.noreply.github.com>
2025-05-06 17:10:49 +0800

    update: add locators and data for product button

--- resources/locators.js
+++ resources/locators.js
@@ -4,20 +4,24 @@ export const locators = {
   buttonLogin: "login-button",
   dashboardTitle: '//*[@data-test="title"]',
   errorBoxAllert: '//*[@data-test="error"]',
+  addBackpackButton: "add-to-cart-sauce-labs-backpack",
+  removeBackpackButton: "remove-sauce-labs-backpack",
 };
 
 export const data = {
   standardUser: "standard_user",
   lockedOutUser: "locked_out_user",
+  problemUser: "problem_user",
   password: "secret_sauce",
   invalidPassword: "wrong_password",
   dashboardTitle: "Products",
+  removeProductText: "Remove",
 };
 
 export const errorMessage = {
   lockedOutUser: "Epic sadface: Sorry, this user has been locked out.",
 };
 
-export const url = {
+export const expectedUrl = {
   dashboardUrl: "https://www.saucedemo.com/inventory.html",
 };
