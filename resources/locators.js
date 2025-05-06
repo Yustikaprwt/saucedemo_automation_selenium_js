acb43de0debd485f4e0133aa97f8d56fd8635998
Yustikaprwt <83440868+Yustikaprwt@users.noreply.github.com>
2025-05-06 13:31:20 +0800

    add login page locators

---
+++ resources/locators.js
@@ -0,0 +1,17 @@
+export const locators = {
+  username: "user-name",
+  password: "password",
+  buttonLogin: "login-button",
+  dashboardTitle: '//*[@data-test="title"]',
+};
+
+export const data = {
+  standardUser: "standard_user",
+  password: "secret_sauce",
+  invalidPassword: "wrong_password",
+  dashboardTitle: "Products",
+};
+
+export const url = {
+  dashboardUrl: "https://www.saucedemo.com/inventory.html",
+};
