3829120e538a6f0b19582103232e315002a0450c
Yustikaprwt <83440868+Yustikaprwt@users.noreply.github.com>
2025-05-06 17:12:46 +0800

    update: update import and remove configuration for chrome webdriver

--- tests/loginValid.test.js
+++ tests/loginValid.test.js
@@ -2,7 +2,7 @@ const { By } = require("selenium-webdriver");
 const chrome = require("selenium-webdriver/chrome");
 const assert = require("assert");
 const driver = require("../resources/driver.js");
-const { locators, data, url } = require("../resources/locators.js");
+const { locators, data, expectedUrl } = require("../resources/locators.js");
 require("dotenv").config();
 const BASE_URL = process.env.BASE_URL;
 
@@ -10,14 +10,7 @@ describe("Login with valid credentials", async function () {
   this.timeout(30000);
 
   before(async function () {
-    const option = new chrome.Options();
-
-    option.addArguments("--disable-save-password-bubble");
-
-    option.setUserPreferences({
-      credentials_enable_service: false,
-      "profile.password_manager_enabled": false,
-    });
+    console.log("Running test login with valid credentials");
   });
 
   it("LGN_001 - Login account as user with 'standard_user' username and valid password", async function () {
@@ -32,10 +25,11 @@ describe("Login with valid credentials", async function () {
     const getURL = await driver.getCurrentUrl();
     const title = await driver.findElement(By.xpath(locators.dashboardTitle));
     const getTitle = await title.getText();
+    const getExpectedUrl = await expectedUrl.dashboardUrl;
 
-    assert.equal(getURL, url.dashboardUrl);
+    assert.equal(getURL, getExpectedUrl);
     assert.equal(getTitle, data.dashboardTitle);
   });
 
-  after(async () => await driver.quit());
+  after(async () => await driver.close());
 });
