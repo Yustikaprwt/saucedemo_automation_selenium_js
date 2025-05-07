8c4944e8cf3c537d00af07d5d9b8b9cb11ee538d
Yustikaprwt <83440868+Yustikaprwt@users.noreply.github.com>
2025-05-07 14:41:16 +0800

    test(product): PRD_002 - Display the products price details on the Inventory page

--- tests/product.test.js
+++ tests/product.test.js
@@ -23,7 +23,7 @@ describe("Test the functionality of view product and product detail", async func
     assert.equal(getUrl, getExpectedUrl);
   });
 
-  it.only("PRD_001 - Display all products on the Inventory page", async () => {
+  it("PRD_001 - Display all products on the Inventory page", async () => {
     const detailProductName = await driver.findElements(
       By.xpath(locators.detailProductName)
     );
@@ -47,6 +47,33 @@ describe("Test the functionality of view product and product detail", async func
 
     for (const expected of expectedListProductName) {
       assert.ok(actualProductName.includes(expected));
+    }
+  });
+
+  it.only("PRD_002 - Display the products price details on the Inventory page", async () => {
+    const productsPrice = await driver.findElements(
+      By.xpath(locators.detailPrice)
+    );
+
+    assert.strictEqual(productsPrice.length, 6);
+
+    const expectedProductsPrice = [
+      data.backpackPrice,
+      data.bikeLightPrice,
+      data.boltTshirtPrice,
+      data.fleeceJacketPrice,
+      data.onesiePrice,
+      data.tshirtPrice,
+    ];
+
+    const actualProductsPrice = [];
+    for (const price of productsPrice) {
+      const text = await price.getText();
+      actualProductsPrice.push(text);
+    }
+
+    for (const expected of expectedProductsPrice) {
+      assert.ok(actualProductsPrice.includes(expected));
     }
   });
 
