6146e32be5633b88af35b3c85c711c736a8cce5b
Yustikaprwt <83440868+Yustikaprwt@users.noreply.github.com>
2025-05-07 14:29:45 +0800

    test(product): PRD_001 - Display all products on the Inventory page

---
+++ tests/product.test.js
@@ -0,0 +1,54 @@
+const { By } = require("selenium-webdriver");
+const assert = require("assert");
+const driver = require("../resources/driver.js");
+const { locators, data, expectedUrl } = require("../resources/locators.js");
+require("dotenv").config();
+const BASE_URL = process.env.BASE_URL;
+
+describe("Test the functionality of view product and product detail", async function () {
+  this.timeout(30000);
+
+  before(async () => {
+    await driver.get(BASE_URL);
+
+    await driver
+      .findElement(By.id(locators.username))
+      .sendKeys(data.standardUser);
+    await driver.findElement(By.id(locators.password)).sendKeys(data.password);
+    await driver.findElement(By.id(locators.buttonLogin)).click();
+
+    const getUrl = await driver.getCurrentUrl();
+    const getExpectedUrl = await expectedUrl.dashboardUrl;
+
+    assert.equal(getUrl, getExpectedUrl);
+  });
+
+  it.only("PRD_001 - Display all products on the Inventory page", async () => {
+    const detailProductName = await driver.findElements(
+      By.xpath(locators.detailProductName)
+    );
+
+    assert.strictEqual(detailProductName.length, 6);
+
+    const expectedListProductName = [
+      data.backpackDetailName,
+      data.bikeLightDetailName,
+      data.boltTshirtDetailName,
+      data.fleeceJacketDetailName,
+      data.onesieDetailName,
+      data.tshirtDetailName,
+    ];
+
+    const actualProductName = [];
+    for (const el of detailProductName) {
+      const text = await el.getText();
+      actualProductName.push(text);
+    }
+
+    for (const expected of expectedListProductName) {
+      assert.ok(actualProductName.includes(expected));
+    }
+  });
+
+  after(async () => driver.close());
+});
