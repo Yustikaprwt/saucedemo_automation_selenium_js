c6dda3e9006d7fceb6dced9eaafc3485086d8d59
Yustikaprwt <83440868+Yustikaprwt@users.noreply.github.com>
2025-05-22 17:42:33 +0800

    refactor: extract expected url to separate file

---
+++ resources/expectedUrl.js
@@ -0,0 +1,6 @@
+export const expectedUrl = {
+  inventoryUrl: "https://www.saucedemo.com/inventory.html",
+  backpackProductUrl: "https://www.saucedemo.com/inventory-item.html?id=4",
+  oneSieProductUrl: "https://www.saucedemo.com/inventory-item.html?id=2",
+  cartPage: "https://www.saucedemo.com/cart.html",
+};
