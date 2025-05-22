b0148fee603b3c5ad2319c88dc18d8d1bdc10884
Yustikaprwt <83440868+Yustikaprwt@users.noreply.github.com>
2025-05-22 17:48:05 +0800

    test(login): update TC_LGN_001 - fix imports, locators property, and test naming convention

--- tests/loginValid.test.js
+++ tests/loginValid.test.js
@@ -1,7 +1,9 @@
 const { By } = require("selenium-webdriver");
 const assert = require("assert");
 const createDriver = require("../resources/driver.js");
-const { locators, data, expectedUrl } = require("../resources/locators.js");
+const { locators } = require("../resources/locators.js");
+const { data } = require("../resources/data.js");
+const { expectedUrl } = require("../resources/expectedUrl.js");
 require("dotenv").config();
 const BASE_URL = process.env.BASE_URL;
 
@@ -15,27 +17,32 @@ describe("Login with valid credentials", async function () {
     console.log("Running test login with valid credentials");
   });
 
-  it("LGN_001 - Login account as user with 'standard_user' username and valid password", async function () {
+  it("TC_LGN_001 - Login account as user with 'standard_user' username and valid password", async function () {
     await driver.get(BASE_URL);
 
     await driver
-      .findElement(By.id(locators.username))
-      .sendKeys(data.standardUser);
-    await driver.findElement(By.id(locators.password)).sendKeys(data.password);
-    await driver.findElement(By.id(locators.buttonLogin)).click();
+      .findElement(By.id(locators.login.username))
+      .sendKeys(data.login.standardUser);
+    await driver
+      .findElement(By.id(locators.login.password))
+      .sendKeys(data.login.password);
+    await driver.findElement(By.id(locators.login.buttonLogin)).click();
 
     const getURL = await driver.getCurrentUrl();
-    const title = await driver.findElement(By.xpath(locators.dashboardTitle));
-    const getTitle = await title.getText();
-    const getExpectedUrl = await expectedUrl.dashboardUrl;
-
+    const getExpectedUrl = expectedUrl.inventoryUrl;
     assert.equal(getURL, getExpectedUrl);
-    assert.equal(getTitle, data.dashboardTitle);
+
+    const getElementTitle = await driver.findElement(
+      By.xpath(locators.title.inventoryTitle)
+    );
+    const getTitle = await getElementTitle.getText();
+    const expectedTitle = data.title.inventoryTitle;
+    assert.equal(getTitle, expectedTitle);
   });
 
   after(async () => {
     if (driver) {
-      await driver.close();
+      await driver.quit();
     }
   });
 });
