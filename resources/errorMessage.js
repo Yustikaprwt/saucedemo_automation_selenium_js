4fbd2146a7fdb515ead6064abf0bb1a6df3d49b5
Yustikaprwt <83440868+Yustikaprwt@users.noreply.github.com>
2025-05-22 17:42:16 +0800

    refactor: extract error messages to separate file

---
+++ resources/errorMessage.js
@@ -0,0 +1,7 @@
+export const errorMessage = {
+  lockedOutUser: "Epic sadface: Sorry, this user has been locked out.",
+  invalidInput:
+    "Epic sadface: Username and password do not match any user in this service",
+  requiredPassword: "Epic sadface: Password is required",
+  requiredUsername: "Epic sadface: Username is required",
+};
