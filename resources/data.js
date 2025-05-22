37cda50a6035460fbe42375c26571b2663142276
Yustikaprwt <83440868+Yustikaprwt@users.noreply.github.com>
2025-05-22 17:40:17 +0800

    refactor: organize test data with nested objects by feature

---
+++ resources/data.js
@@ -0,0 +1,40 @@
+export const data = {
+  login: {
+    standardUser: "standard_user",
+    lockedOutUser: "locked_out_user",
+    problemUser: "problem_user",
+    glitchUser: "performance_glitch_user",
+    errorUser: "error_user",
+    visualUser: "visual_user",
+    invalidUsername: "wrong_username",
+    password: "secret_sauce",
+    invalidPassword: "wrong_password",
+  },
+
+  products: {
+    addTocartTextButton: "Add to cart",
+    backpackDetailName: "Sauce Labs Backpack",
+    bikeLightDetailName: "Sauce Labs Bike Light",
+    boltTshirtDetailName: "Sauce Labs Bolt T-Shirt",
+    fleeceJacketDetailName: "Sauce Labs Fleece Jacket",
+    onesieDetailName: "Sauce Labs Onesie",
+    tshirtDetailName: "Test.allTheThings() T-Shirt (Red)",
+    backpackPrice: "$29.99",
+    bikeLightPrice: "$9.99",
+    boltTshirtPrice: "$15.99",
+    fleeceJacketPrice: "$49.99",
+    onesiePrice: "$7.99",
+    tshirtPrice: "$15.99",
+    backpackDesc:
+      "carry.allTheThings() with the sleek, streamlined Sly Pack that melds uncompromising style with unequaled laptop and tablet protection.",
+  },
+
+  button: {
+    removeProductText: "Remove",
+    backToProductsButton: "Back to products",
+  },
+
+  title: {
+    inventoryTitle: "Products",
+  },
+};
