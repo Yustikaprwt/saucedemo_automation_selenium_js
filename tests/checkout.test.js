25e03b5b4003ed1d68277c4e5de7de75cc0ff17d
Yustikaprwt <83440868+Yustikaprwt@users.noreply.github.com>
2025-05-23 19:57:13 +0800

    test(checkout): TC_CHECKOUT_002 - Successfully redirected to the checkout page

--- tests/checkout.test.js
+++ tests/checkout.test.js
@@ -55,3 +55,48 @@ describe("Test the functionality of checkout feature with empty product", async 
     }
   });
 });
+
+describe("Test the functionality of the checkout feature with at least one product in the cart.", async function () {
+  this.timeout(30000);
+  let driver;
+
+  beforeEach(async () => {
+    driver = await createDriver();
+    await driver.get(BASE_URL);
+
+    await driver
+      .findElement(By.id(locators.login.username))
+      .sendKeys(data.login.standardUser);
+    await driver
+      .findElement(By.id(locators.login.password))
+      .sendKeys(data.login.password);
+    await driver.findElement(By.id(locators.login.buttonLogin)).click();
+
+    const getUrl = await driver.getCurrentUrl();
+    const getExpectedUrl = expectedUrl.inventoryUrl;
+    assert.equal(getUrl, getExpectedUrl);
+
+    await driver
+      .findElement(By.id(locators.products.addBackpackButton))
+      .click();
+
+    await driver.findElement(By.xpath(locators.cart.cartIcon)).click();
+    const getCartUrl = await driver.getCurrentUrl();
+    const cartUrl = expectedUrl.cartPage;
+    assert.equal(getCartUrl, cartUrl);
+  });
+
+  it("TC_CHECKOUT_002 - Successfully redirected to the checkout page by clicking the 'Checkout' button on the Cart page when the cart contains products", async () => {
+    await driver.findElement(By.id(locators.button.checkoutButton)).click();
+
+    const getUrl = await driver.getCurrentUrl();
+    const url = expectedUrl.checkoutUrl;
+    assert.equal(getUrl, url);
+  });
+
+  afterEach(async () => {
+    if (driver) {
+      await driver.close();
+    }
+  });
+});
