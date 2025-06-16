252618673aa436e20b44b285efb604f9c6f5894f
Yustikaprwt <83440868+Yustikaprwt@users.noreply.github.com>
2025-06-16 22:50:36 +0800

    feat(pom): add methods to check login functionality and apply implicit wait for all elements

--- page/loginPage.js
+++ page/loginPage.js
@@ -1,4 +1,4 @@
-const { By } = require("selenium-webdriver");
+const { By, until } = require("selenium-webdriver");
 const { locators } = require("../resources/locators.js");
 
 class LoginPage {
@@ -7,24 +7,63 @@ class LoginPage {
     this.usernameField = By.id(locators.login.username);
     this.passwordField = By.id(locators.login.password);
     this.loginButton = By.id(locators.login.buttonLogin);
+    this.errorMessage = By.css(locators.message.errorBoxAllert);
   }
 
-  async inputUsername(username) {
-    await this.driver.findElement(this.usernameField).sendKeys(username);
+  async inputUsername(username, timeout = 5000) {
+    const getUsernameField = await this.driver.wait(
+      until.elementLocated(this.usernameField),
+      timeout
+    );
+
+    await this.driver.wait(until.elementIsVisible(getUsernameField), timeout);
+    await getUsernameField.sendKeys(username);
   }
 
-  async inputPassword(password) {
-    await this.driver.findElement(this.passwordField).sendKeys(password);
+  async inputPassword(password, timeout = 5000) {
+    const getPasswordField = await this.driver.wait(
+      until.elementLocated(this.passwordField),
+      timeout
+    );
+
+    await this.driver.wait(until.elementIsVisible(getPasswordField), timeout);
+    await getPasswordField.sendKeys(password);
   }
 
-  async clickLoginButton() {
-    await this.driver.findElement(this.loginButton).click();
+  async clickLoginButton(timeout = 5000) {
+    const button = await this.driver.wait(
+      until.elementLocated(this.loginButton),
+      timeout
+    );
+
+    await this.driver.wait(until.elementIsVisible(button), timeout);
+    await button.click();
   }
 
   async login(username, password) {
     await this.inputUsername(username);
     await this.inputPassword(password);
     await this.clickLoginButton();
+  }
+
+  async loginWithEmptyPassword(username) {
+    await this.inputUsername(username);
+    await this.clickLoginButton();
+  }
+
+  async loginWithEmptyUsername(password) {
+    await this.inputPassword(password);
+    await this.clickLoginButton();
+  }
+
+  async getErrorMessageText(timeout = 5000) {
+    const errorElement = await this.driver.wait(
+      until.elementLocated(this.errorMessage),
+      timeout
+    );
+
+    await this.driver.wait(until.elementIsVisible(errorElement), timeout);
+    return await errorElement.getText();
   }
 }
 
